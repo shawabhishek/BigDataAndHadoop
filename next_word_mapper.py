fc2f55ec17fc33aed98001db13a1026ea5852f6d
Alan Shaw <alanshaw@runbox.com>
2016-05-13 14:30:07 -0400

    updated next word mapper and reducer

--- next_word_mapper.py
+++ next_word_mapper.py
@@ -1,15 +1,36 @@
 #!/usr/bin/python
 
-# Alan Shaw
-# Assignment 3.2 Next Word Recommendations
+# Written by Alan Shaw as an assignment for the Big Data and Hadoop
+# course at http://www.knowbigdata.com. For use with Hadoop Streaming.
+
+# Based on the content from a very large text archive, for each word
+# prepare a recommmendation list of the top 5 next words.
 
 # Mapper
+
+# Output all bigrams (consecutive two word combinations),
+# including those that cross over lines
 
 import fileinput
 import re
 
+next_word = ''
+
 for line in fileinput.input():
-	line = re.sub('[^a-z ]', '', line.lower())
+	# strip out everything except letters, spaces, and punctuation
+	line = re.sub('[^a-z 0-9]', '', line.lower())
 	words = line.split()
+	# first output bigrams that cross lines
+	this_word = next_word
+	if len(words) > 0:
+		next_word = words[0]
+	else:
+		next_word = ''
+	if len(this_word) > 0 and len(next_word) > 0:
+		print(this_word + '\t' + next_word)
+	# now iterate through the rest of the line
 	for i in range(len(words) - 1): 
-		print(words[i] + '\t' + words[i + 1])
+		this_word = words[i]
+		next_word = words[i + 1]
+		if len(this_word) > 0 and len(next_word) > 0:
+			print(this_word + '\t' + next_word)
--- next_word_reducer.py
+++ next_word_reducer.py
@@ -1,27 +1,67 @@
 #!/usr/bin/python
 
-# Alan Shaw
-# Assignment 3.2 Next Word bigrams
+# Written by Alan Shaw as an assignment for the Big Data and Hadoop
+# course at http://www.knowbigdata.com. For use with Hadoop Streaming.
+
+# Based on the content from a very large text archive, for each word
+# prepare a recommmendation list of the top 5 next words.
 
 # Reducer
 
-import fileinput
+# Expects bigrams sorted by both words but partitioned only by the first word
 
-bigrams = {} 
-recommendations = {}
+# Use this generic option to sort on both fields:
+# -D stream.num.map.output.key.fields=2
+
+# Use the KeyFieldBasedPartitioner to partition on only the first field:
+# -D mapreduce.partition.keypartitioner.options=-k1
+# -partitioner org.apache.hadoop.mapred.lib.KeyFieldBasedPartitioner 
+
+import fileinput
+import heapq
+
+# use a heap of tuples
+# https://docs.python.org/3.5/library/heapq.html
+# "Heap elements can be tuples."
+
+def qualify(recommendations, counter, next_word):
+	if len(recommendations) < 5:
+		if counter > 0 and len(next_word) > 0:
+			heapq.heappush(recommendations, (counter, next_word))
+	elif counter > recommendations[0][0]:
+		heapq.heappop(recommendations)
+		heapq.heappush(recommendations, (counter, next_word))
+
+def output(recommendations, this_word):
+	if len(this_word) > 0 and len(recommendations) > 0:
+		output_list = list()
+		while len(recommendations) > 0:
+			output_list.append(heapq.heappop(recommendations)[1])
+		print(this_word + ':\t' + ', '.join(output_list))
+
+
+this_word = ''
+recommendations = []
+counter = 0
+next_word = ''
 
 for line in fileinput.input():
 	words = line.split()
-	if (words[0], words[1]) not in bigrams:
-		bigrams[(words[0], words[1])] = 0
-	bigrams[(words[0], words[1])] += 1
-
-for bigram in sorted(bigrams, key=bigrams.__getitem__)[::-1]:
-	if bigram[0] not in recommendations:
-		recommendations[bigram[0]] = set()
-	if len(recommendations[bigram[0]]) < 5:
-		recommendations[bigram[0]].add(bigram[1])
-
-for key in recommendations:
-	if len(recommendations[key]) == 5:
-		print(key + '\t' + ', '.join(recommendations[key]))
+	if words[0] != this_word: # new key word
+		qualify(recommendations, counter, next_word)
+		# before moving on, output this word and its recommendations
+		output(recommendations, this_word)
+		this_word = words[0]
+		next_word = words[1]
+		counter = 1
+	elif words[1] != next_word: # new next word to count
+		# before moving on, decide whether to add the recommendation
+		qualify(recommendations, counter, next_word)
+		next_word = words[1]
+		counter = 1
+	else:
+		counter += 1
+	
+qualify(recommendations, counter, next_word)
+# output the last word and its recommendations
+output(recommendations, this_word)
